0f3bf97d5a13a63867a30a8503eb4de616c1d968
zhufucdev <1095291434@qq.com>
2023-03-26 10:22:51 +0800

    Typo

--- util.py
+++ util.py
@@ -17,8 +17,9 @@ def plus(vector: Tuple[float, float], option: Tuple[float, float]):
     return vector[0] + option[0], vector[1] + option[1]
 
 
-def multiply(vector: Tuple[float, float], option: float):
-    return vector[0] * option, vector[1] * option
+def multiply(vector: Tuple[float, float], factor: float):
+    return vector[0] * factor, vector[1] * factor
+
 
 def is_positive(vector: Tuple[float, float]):
     return vector[0] > 0 and vector[1] > 0
